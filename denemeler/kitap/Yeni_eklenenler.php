c6d838ab3811fc0473527f0e8bb0b77b7397bae0
İrfan Evrens <irfanevrens@gmail.com>
2015-02-15 16:54:02 +0200

    ufak

--- denemeler/kitap/Yeni_eklenenler.php
+++ denemeler/kitap/Yeni_eklenenler.php
@@ -1,5 +1,8 @@
 <?php
 
+use CamlicaKitapApi\KitapYeniEklenenler;
+use CamlicaKitapApi\Model\Istek\Kitap\YeniEklenenlerIstek;
+
 require_once '../../vendor/autoload.php';
 
 error_reporting(E_ALL);
@@ -7,11 +10,13 @@ error_reporting(E_ALL);
 // hash bilgisini buraya yazınız.
 $hash = '';
 
-$istek = new \CamlicaKitapApi\Model\Istek\Kitap\YeniEklenenlerIstek($hash);
+$istek = new YeniEklenenlerIstek($hash);
+
+$istek->adet = 10;
 
 try {
 
-	$cevap =  \CamlicaKitapApi\KitapYeniEklenenler::get($istek);
+	$cevap =  KitapYeniEklenenler::get($istek);
 
 	echo '<pre>';
 	print_r($cevap);
--- src/CamlicaKitapApi/Model/Istek/Kitap/YeniEklenenlerIstek.php
+++ src/CamlicaKitapApi/Model/Istek/Kitap/YeniEklenenlerIstek.php
@@ -4,10 +4,14 @@ use CamlicaKitapApi\Model\Istek\Temel\GenelIstek;
 
 class YeniEklenenlerIstek extends GenelIstek
 {
+	// kaç adet sonuç dönecek
+	public $adet;
+
 	public function getArray()
 	{
 		return [
-			'hash' => $this->hash
+			'hash' => $this->hash,
+			'adet' => $this->adet
 		];
 	}
 }
