d3818feb294c83124e020807339ccf9c6c8530b2
İrfan Evrens <irfanevrens@gmail.com>
2015-02-16 08:57:56 +0200

    ufak

--- src/CamlicaKitapApi/Model/Istek/Temel/TemelListeIstek.php
+++ src/CamlicaKitapApi/Model/Istek/Temel/TemelListeIstek.php
@@ -2,6 +2,8 @@
 
 class TemelListeIstek extends TemelIstek
 {
+	public $yazarId;
+	public $kategoriId;
 	public $siralamaKey;
 	public $siralamaValue = 'asc';
 	public $baslangic = 0;
@@ -10,6 +12,8 @@ class TemelListeIstek extends TemelIstek
 	public function getArray()
 	{
 		return array_merge(parent::getArray(), [
+			'yazar_id' => $this->yazarId,
+			'kategori_id' => $this->kategoriId,
 			'siralama_key' => $this->siralamaKey,
 			'siralama_value' => $this->siralamaValue,
 			'baslangic' => $this->baslangic,
